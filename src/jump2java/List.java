a196de33b61483da1cf598935c4a286b18a24188
korthetree <jsjake4770@gmail.com>
2021-02-05 02:17:55 +0900

    New files

--- src/jump2java/List.java
+++ src/jump2java/List.java
@@ -1,7 +1,18 @@
 package jump2java;
 
+import java.util.ArrayList;
+
 public class List {
     public static void main(String[] args) {
-        ArrayList pitches
+        ArrayList pitches = new ArrayList();
+        pitches.add("138");
+        pitches.add("129");
+        pitches.add("142");
+
+        System.out.println(pitches.get(1));
+        System.out.println(pitches.size());
+        System.out.println(pitches.contains("142"));
+        System.out.println(pitches.remove("129"));
+        System.out.print(pitches.remove(0));
     }
 }
